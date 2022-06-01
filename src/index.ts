778a2b59fe2c4be1fd28006e5fd2fd6ccf245a51
Lena Vu <mywayrabota2@gmail.com>
2022-06-01 11:46:30 +0100

    remove element

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,9 @@ const queue = [12, 8, 4, 35, 45, 20] // random array of numbers
 
 queue.forEach((e) => insert(heap, e)) // create a heap
 
-console.log(heap)
+let heapSize = heap.length;
 
-;[...heap].forEach((e) => {
-    console.log(remove(heap)) // remove elements with a highest priority
-})
+while (heapSize) {
+    console.log(remove(heap)) // remove element with a highest priority
+    heapSize--
+}
